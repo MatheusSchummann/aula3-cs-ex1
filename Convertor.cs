de94c15adcb3a9a3947f3061637371c44a065b7d
autologon <autologon@RS01-H222-29>
2024-08-19 21:41:41 -0300

    Adicionar arquivos de projeto.

---
+++ Convertor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aulap3
+{
+    public class Convertor
+    {
+       
+        public List<double> transferirdouble(List<string> listaS)
+        {
+            List<double> listaD = listaS.ConvertAll(double.Parse);
+
+            return listaD;
+        }
+        public double resulfinal(List<double> lista2) 
+        {
+            double aux = 0;
+            foreach(double soma in lista2)
+            {
+                aux += soma;
+            }
+            return aux/3.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aulap3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+        }
+
+        public Program()
+        {
+            Convertor convertor = new Convertor();
+            double vtotal = 0;
+
+            List<string> list = new List<string>();
+            List<double> list2 = new List<double>();
+
+            convertor.transferirdouble(list);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Digite a nota 0{0}: ", i + 1);
+                list.Add(Console.ReadLine());
+            }
+
+            list2 = convertor.transferirdouble(list);
+            vtotal = convertor.resulfinal(list2);
+     
+            Console.WriteLine("A média do aluno foi: {0}", vtotal);
+            Console.ReadKey();
+        }
+    }
+}
